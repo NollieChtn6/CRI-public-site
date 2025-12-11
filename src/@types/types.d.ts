21acc38bf21fd83343e1f01df9719bc561bec150
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-12-11 12:46:41 +0100

    update types

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -14,3 +14,33 @@ export interface NavLinkInterface {
   label: string;
   path: string;
 }
+
+export interface ArticleInterface {
+  id: string;
+  title: string;
+  slug: string;
+  author: string;
+  date: string;
+  summary: string;
+  "cover-picture-url": string;
+  "cover-picture-alt": string;
+  content: string;
+}
+
+export interface GalleryInterface {
+  id: string;
+  title: string;
+  slug: string;
+  department: number;
+  date: string;
+  author: string;
+  description: string;
+  content: string;
+  category: string;
+  images: GalleryImage[];
+}
+
+export interface GalleryImage {
+  url: string;
+  caption: string;
+}
