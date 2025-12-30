21b50a8ade09eaa81c8b325a7d732a64df5a02d9
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-12-30 15:46:42 +0100

    create carousel for hero section

---
+++ src/components/pictures/hero/HeroCarouselClient.tsx
@@ -0,0 +1,34 @@
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import type { Picture } from "@/@types/types";
+
+export default function HeroCarouselClient({ pictures }: { pictures: Picture[] }) {
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [
+    Autoplay({
+      delay: 3500,
+      stopOnInteraction: false,
+      stopOnMouseEnter: true,
+    }),
+  ]);
+
+  return (
+    <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-white/10" ref={emblaRef}>
+      <div className="flex">
+        {pictures.map((picture) => (
+          <div
+            key={picture.url}
+            className="flex-[0_0_100%] relative cursor-pointer"
+            title={` © Crédit photo : ${picture.author}`}
+          >
+            <img
+              src={picture.url}
+              alt={picture.alt}
+              className="w-full h-full object-cover aspect-13/9"
+              loading="eager"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
