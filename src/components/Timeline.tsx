3c9f2a67b09a90ff9819bca1e5766a83a0d01878
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2026-01-03 15:47:52 +0100

    update wording

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -19,43 +19,45 @@ export default function Timeline() {
             className="bg-shark-700 absolute left-5 top-0 h-full z-0"
           />
 
-          {(milestones as YearEntry[]).map((yearEntry) => (
-            <>
-              <h3 className="text-2xl font-bold text-shark-50 mb-6 -translate-x-15 lg:-translate-x-20">
-                {yearEntry.year}
-              </h3>
-              <div key={yearEntry.year} className="mb-12">
-                {yearEntry.events.map((event) => {
-                  const IconComponent: IconType | undefined =
-                    (FaIcons as { [key: string]: IconType })[event.iconName] ||
-                    (IoIcons as { [key: string]: IconType })[event.iconName] ||
-                    (MdIcons as { [key: string]: IconType })[event.iconName] ||
-                    (PiIcons as { [key: string]: IconType })[event.iconName];
+          {(milestones as YearEntry[])
+            .map((yearEntry) => (
+              <>
+                <h3 className="text-2xl font-bold text-shark-50 mb-6 -translate-x-15 lg:-translate-x-20">
+                  {yearEntry.year}
+                </h3>
+                <div key={yearEntry.year} className="mb-12">
+                  {yearEntry.events.map((event) => {
+                    const IconComponent: IconType | undefined =
+                      (FaIcons as { [key: string]: IconType })[event.iconName] ||
+                      (IoIcons as { [key: string]: IconType })[event.iconName] ||
+                      (MdIcons as { [key: string]: IconType })[event.iconName] ||
+                      (PiIcons as { [key: string]: IconType })[event.iconName];
 
-                  return (
-                    <div key={event.id} className="relative mb-10 pl-8 space-y-4">
-                      <div className="flex relative z-10 items-center space-y-4">
-                        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-red-orange-500 text-shark-50 shadow ring-[1.5px] ring-shark-50 -translate-x-4/5">
-                          {IconComponent && <IconComponent className="h-5 w-5" />}
+                    return (
+                      <div key={event.id} className="relative mb-10 pl-8 space-y-4">
+                        <div className="flex relative z-10 items-center space-y-4">
+                          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-red-orange-500 text-shark-50 shadow ring-[1.5px] ring-shark-50 -translate-x-4/5">
+                            {IconComponent && <IconComponent className="h-5 w-5" />}
+                          </div>
+                          <h4 className="text-lg lg:text-xl font-bold tracking-tight text-shark-50 ">
+                            {event.title}
+                          </h4>
                         </div>
-                        <h4 className="text-lg lg:text-xl font-bold tracking-tight text-shark-50 ">
-                          {event.title}
-                        </h4>
+
+                        {(event.startDay || event.startMonth) && (
+                          <h5 className="text-sm text-shark-300 -left-30 ml-2 top-3 rounded-xl tracking-tight xl:absolute mb-4">
+                            {formatTimelineDate(event.startDay, event.startMonth, yearEntry.year)}
+                          </h5>
+                        )}
+
+                        <TimelineCard event={event} />
                       </div>
-
-                      {(event.startDay || event.startMonth) && (
-                        <h5 className="text-sm text-shark-300 -left-30 ml-2 top-3 rounded-xl tracking-tight xl:absolute mb-4">
-                          {formatTimelineDate(event.startDay, event.startMonth, yearEntry.year)}
-                        </h5>
-                      )}
-
-                      <TimelineCard event={event} />
-                    </div>
-                  );
-                })}
-              </div>
-            </>
-          ))}
+                    );
+                  })}
+                </div>
+              </>
+            ))
+            .reverse()}
         </div>
       </div>
     </section>
